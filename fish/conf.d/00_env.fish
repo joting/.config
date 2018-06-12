91630131a8f7be3f24b89f6b8d0f0c8f54feba99
Jordan Ting <jordanting@gmail.com>
2018-06-12 16:36:47 +1000

    Ignore contents of fisherman

--- fish/conf.d/00_env.fish
+++ fish/conf.d/00_env.fish
@@ -1,3 +1,7 @@
-fenv source ~/.profile
+#fenv source ~/.profile
+set -x SHELL /usr/bin/fish
+set -x PATH /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin $PATH
+set -x MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
 set -x EDITOR nvim
 set -x VISUAL nvim
