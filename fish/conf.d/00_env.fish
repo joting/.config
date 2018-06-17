4f4889f38a940c996ef002b051c95798ef1acaa4
Jordan Ting <jordanting@gmail.com>
2018-06-17 16:52:11 +1000

    Reverted changes to path relating to linuxbrew

--- fish/conf.d/00_env.fish
+++ fish/conf.d/00_env.fish
@@ -1,7 +1,4 @@
 #fenv source ~/.profile
 set -x SHELL /usr/bin/fish
-set -x PATH /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin $PATH
-set -x MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
-set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
 set -x EDITOR nvim
 set -x VISUAL nvim
