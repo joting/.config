f19e105ba29e4a23c11d4516db2bcef1c48a6ea7
Jordan Ting <jordanting@gmail.com>
2018-06-17 16:59:45 +1000

    Restored changes to path relating to linuxbrew

--- fish/conf.d/00_env.fish
+++ fish/conf.d/00_env.fish
@@ -2,3 +2,6 @@
 set -x SHELL /usr/bin/fish
 set -x EDITOR nvim
 set -x VISUAL nvim
+set -x PATH /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin $PATH
+set -x MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+set -x INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
